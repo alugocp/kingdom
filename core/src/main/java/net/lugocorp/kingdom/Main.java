a309b89649a39a8f35df63262c0ed780ee437d58
Alex Lugo <alugocp@gmail.com>
2024-09-25 18:52:13 -0400

    3D model load/render test

--- core/src/main/java/net/lugocorp/kingdom/Main.java
+++ core/src/main/java/net/lugocorp/kingdom/Main.java
@@ -7,23 +7,80 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.Model;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private BitmapFont font;
     private Texture image;
+    public PerspectiveCamera cam;
+    public CameraInputController camController;
+    public ModelBatch modelBatch;
+    public AssetManager assets;
+    public ModelInstance instance;
+    public Environment environment;
+    public boolean loading;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         image = new Texture("icons.png");
         font = new BitmapFont();
+
+        // 3D stuff
+        modelBatch = new ModelBatch();
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
+        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.position.set(1f, 1f, 1f);
+        cam.lookAt(0,0,0);
+        cam.near = 1f;
+        cam.far = 300f;
+        cam.update();
+
+        camController = new CameraInputController(cam);
+        Gdx.input.setInputProcessor(camController);
+
+        assets = new AssetManager();
+        assets.load("tile.obj", Model.class);
+        loading = true;
+    }
+
+    private void doneLoading() {
+        Model model = assets.get("tile.obj", Model.class);
+        instance = new ModelInstance(model);
+        loading = false;
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+
+        if (loading && assets.update()) {
+            this.doneLoading();
+        }
+        camController.update();
+
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        modelBatch.begin(cam);
+        if (instance != null) {
+            modelBatch.render(instance, environment);
+        }
+        modelBatch.end();
+
         batch.begin();
         batch.draw(image, 140, 210);
         font.draw(batch, "Kingdom Game", 10, 20);
@@ -35,5 +92,7 @@ public class Main extends ApplicationAdapter {
         batch.dispose();
         image.dispose();
         font.dispose();
+        modelBatch.dispose();
+        assets.dispose();
     }
 }
