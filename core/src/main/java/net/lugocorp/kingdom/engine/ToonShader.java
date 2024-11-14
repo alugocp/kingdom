35f9fb8b7f809be0817919f83021375787c2c8ac
Alex Lugo <alugocp@aim.com>
2024-11-13 22:19:43 -0500

    Fixed texture coords

--- core/src/main/java/net/lugocorp/kingdom/engine/ToonShader.java
+++ core/src/main/java/net/lugocorp/kingdom/engine/ToonShader.java
@@ -13,11 +13,11 @@ public class ToonShader implements Shader {
     private ShaderProgram program;
     private RenderContext context;
     private Camera camera;
-    private int u_diffuseTexture;
-    private int u_diffuseColor;
     private int u_projViewTrans;
     private int u_worldTrans;
     private int u_diffuseUVTransform;
+    private int u_diffuseTexture;
+    private int u_diffuseColor;
 
     @Override
     public void init() {
@@ -27,10 +27,10 @@ public class ToonShader implements Shader {
         if (!this.program.isCompiled()) {
             throw new GdxRuntimeException(this.program.getLog());
         }
-        this.u_projViewTrans = this.program.getUniformLocation("u_projViewTrans");
         this.u_worldTrans = this.program.getUniformLocation("u_worldTrans");
-        this.u_diffuseTexture = this.program.getUniformLocation("u_diffuseTexture");
+        this.u_projViewTrans = this.program.getUniformLocation("u_projViewTrans");
         this.u_diffuseUVTransform = this.program.getUniformLocation("u_diffuseUVTransform");
+        this.u_diffuseTexture = this.program.getUniformLocation("u_diffuseTexture");
         this.u_diffuseColor = this.program.getUniformLocation("u_diffuseColor");
     }
 
@@ -44,19 +44,22 @@ public class ToonShader implements Shader {
         this.camera = camera;
         this.context = context;
         this.program.begin();
-        this.program.setUniformMatrix("u_projViewTrans", camera.combined);
+        this.program.setUniformMatrix(this.u_projViewTrans, camera.combined);
     }
 
     @Override
     public void render(Renderable renderable) {
-        this.program.setUniformMatrix("u_worldTrans", renderable.worldTransform);
-        renderable.meshPart.render(this.program);
+        // Set object uniforms
+        this.program.setUniformMatrix(this.u_worldTrans, renderable.worldTransform);
+
+        // Set material uniforms
         TextureAttribute diffuse = (TextureAttribute) renderable.material.get(TextureAttribute.Diffuse);
-        this.program.setUniformi("u_diffuseTexture", this.context.textureBinder.bind(diffuse.textureDescription));
-        this.program.setUniformf("u_diffuseUVTransform", diffuse.offsetU, diffuse.offsetV, diffuse.scaleU,
+        this.program.setUniformi(this.u_diffuseTexture, this.context.textureBinder.bind(diffuse.textureDescription));
+        this.program.setUniformf(this.u_diffuseUVTransform, diffuse.offsetU, diffuse.offsetV, diffuse.scaleU,
                 diffuse.scaleV);
         ColorAttribute color = (ColorAttribute) renderable.material.get(ColorAttribute.Diffuse);
-        this.program.setUniformf("u_diffuseColor", color.color);
+        this.program.setUniformf(this.u_diffuseColor, color.color);
+        renderable.meshPart.render(this.program);
     }
 
     @Override
