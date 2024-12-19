e9c4961fcf9b3a34e71797c74d308e29191578bd
Alex Lugo <alugocp@aim.com>
2024-12-19 05:11:02 -0500

    Mod asset extraction

--- core/src/main/java/net/lugocorp/kingdom/ui/views/LoadingGameView.java
+++ core/src/main/java/net/lugocorp/kingdom/ui/views/LoadingGameView.java
@@ -3,8 +3,14 @@ import net.lugocorp.kingdom.engine.Graphics;
 import net.lugocorp.kingdom.game.events.AllEventHandlers;
 import net.lugocorp.kingdom.utils.ModLoader;
 import net.lugocorp.kingdom.utils.math.Coords;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.function.Consumer;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 /**
  * View for when we're loading a new game
@@ -35,10 +41,45 @@ public class LoadingGameView implements View {
             ModLoader mods = new ModLoader();
             for (String filepath : mods.getMods()) {
                 System.out.println(String.format("Loading mod %s...", filepath));
+
+                // Load mod code
                 try {
                     mods.loadMod(filepath, this.events);
                 } catch (Exception e) {
                     System.err.println(String.format("Error while loading mod %s", filepath));
+                    e.printStackTrace();
+                    continue;
+                }
+
+                try {
+                    // Set up the mod unzip site
+                    File site = Gdx.files.external("kingdom/extracted").file();
+                    if (site.exists()) {
+                        site.delete();
+                    }
+                    site.mkdirs();
+
+                    // Unzip mod assets
+                    ZipEntry entry = null;
+                    ZipInputStream input = new ZipInputStream(new FileInputStream(filepath));
+                    while ((entry = input.getNextEntry()) != null) {
+                        File file = Gdx.files.external(String.format("kingdom/extracted/%s", entry.getName())).file();
+                        if (entry.isDirectory()) {
+                            file.mkdir();
+                        } else {
+                            file.createNewFile();
+                            int len = 0;
+                            byte[] buffer = new byte[1024];
+                            FileOutputStream output = new FileOutputStream(file);
+                            while ((len = input.read(buffer)) > 0) {
+                                output.write(buffer, 0, len);
+                            }
+                            output.close();
+                        }
+                    }
+                    input.close();
+                } catch (Exception e) {
+                    System.err.println(String.format("Did not load any assets from mod %s", filepath));
                     e.printStackTrace();
                 }
             }
