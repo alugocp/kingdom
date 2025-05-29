86893a26b898c3fe5e49e277e0bcad724852df7e
Alex Lugo <alugocp@aim.com>
2025-05-29 10:09:44 -0400

    Fixed issue with WorldGenOptions

--- core/src/main/java/net/lugocorp/kingdom/game/world/WorldGenOptions.java
+++ core/src/main/java/net/lugocorp/kingdom/game/world/WorldGenOptions.java
@@ -4,7 +4,7 @@ package net.lugocorp.kingdom.game.world;
  * Contains parameters for World generation
  */
 public class WorldGenOptions {
-    public WorldGenOptions.WorldSize size = WorldGenOptions.WorldSize.SMALL;
+    public WorldGenOptions.WorldSize size = WorldGenOptions.WorldSize.values()[0];
     public String seed;
 
     public WorldGenOptions(String seed) {
@@ -15,7 +15,7 @@ public class WorldGenOptions {
      * Represents the possible size values for World generation
      */
     public static enum WorldSize {
-        TEST("Test", 10, 5), SMALL("Small", 50, 50), MEDIUM("Medium", 150, 150), LARGE("Large", 250, 250);
+        SMALL("Small", 50, 50), MEDIUM("Medium", 150, 150), LARGE("Large", 250, 250);
         public final String label;
         public final int w;
         public final int h;
@@ -30,7 +30,6 @@ public class WorldGenOptions {
          * Returns the nth value from the enum
          */
         public static WorldGenOptions.WorldSize fromIndex(int index) {
-            // TODO this isn't working, TEST still returns a larger map size
             return WorldGenOptions.WorldSize.values()[index];
         }
     }
